610fe87a1d218ffc4e88367f646a35cc7562af04
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2017-03-16 09:16:28 -0500

    Log in is ready for being used

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root to: 'welcome#index'
+  #root to: 'welcome#index'
+
+  authenticated :user do
+   root to: 'welcome#home'
+  end
+
+  unauthenticated :user do
+    root to: 'welcome#index' 
+  end
 
 end
