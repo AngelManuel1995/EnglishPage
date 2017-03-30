8fdf48f096616b64fe40664b4a9a093c772059e1
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2017-03-30 12:52:36 -0500

    Ready multiple layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,15 @@
 class WelcomeController < ApplicationController
 
+  ##This action will be execute when the user isn't registired
   def index
   end
-  
+  ##This action will be execute when the is inside if the page
   def home
-    
+  end
+
+  def set_layout
+    return "landing" if action_name == "home"  
+    super
   end
 
 end
