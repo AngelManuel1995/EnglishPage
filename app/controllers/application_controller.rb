ed5394b1608debb8ab60f975067469dd5ed8e147
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2017-03-30 21:41:17 -0500

    Documentation of multiple layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  ##Method that executes or shows at display the value returned by set_layout method  
   layout :set_layout
 
+  ##This indicates that methods are protected
   protected
     
+    ##This method returs the layout that we will show at display, we will take advantage that everything inherits of ApplicationController 
     def set_layout
       "application"     
     end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,9 @@ class WelcomeController < ApplicationController
   ##This action will be execute when the is inside if the page
   def home
   end
-
+  
+  ##This method returns which layout we will use, taking advantage of action_name
+  ## action_name: This method indentifies which acction is executing.
   def set_layout
     return "landing" if action_name == "home"  
     super
