6ba0fab9b020f0ba71bc07919b262af26d376a28
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2017-04-08 12:10:58 -0500

    Article crud ended

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,21 @@
 class ArticlesController < ApplicationController
 
+   before_action :set_article, only:[:show,:edit, :update, :destroy]
+
    def index
      @articles = Article.all
    end
    
+   def show
+   end
 
    def new
      @article = Article.new   
    end
 
+   def edit
+   
+   end
 
    def create
      @article = Article.new(article_params)
@@ -23,10 +30,33 @@ class ArticlesController < ApplicationController
 
    end
 
+   def update
+     
+     respond_to do |format|
+       if @article.update(article_params)
+         format.html {redirect_to articles_path, notice:'Article updated sucessful'}
+       else
+         format.html {render 'edit'}
+       end
+ 
+     end   
+
+   end
+
+   def destroy 
+     @article.destroy
+     respond_to do |format|
+       format.html{redirect_to articles_path, notice:'Article deleted sucessful'} 
+     end
+   end
+
    private 
    
    def article_params
      params.require(:article).permit(:title, :body)
    end
 
+   def set_article
+     @article = Article.find(params[:id])
+   end
 end
